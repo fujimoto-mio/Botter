d3b3e3cc43efeab3f5183c4446f6b68adc94c2d6
fujimoto-mio <tuyopen_t@yahoo.co.jp>
2014-05-18 21:42:29 +0900

    Update setting.php
    ReWrite

--- setting.php
+++ setting.php
@@ -1,16 +1,11 @@
 <?php
 //設定
-//$screen_name = "MIO"; //botのid名
-//$consumer_key = "TMRwLJaDaGIptASJGHT35w"; // Consumer keyの値
-//$consumer_secret = "YJ8P6rK8R4GxPRbiabxwj3ko1qcfU8XMPHoUr8Wlw"; // Consumer secretの値
-//$access_token = "64417503-oUymW9BEKYX1XS9lONMQKJEjpvP9G8nRFMUzbdD53"; // Access Tokenの値
-//$access_token_secret = "gToMmkQpc6FzxFhSxsspIdcHIEvVonnP1XtvzIywg"; // Access Token Secretの値
 
-$screen_name = "MIO"; //botのid名
-$consumer_key = "mW3bKYkuBB64qTLATP5wA"; // Consumer keyの値
-$consumer_secret = "QiNB8GOjHWDea7Rg7oPJdEa4jP6htSZ0uzsvl4zes"; // Consumer secretの値
-$access_token = "555964722-H6Qsrr8SJFlZ2r1Z7D6GG3YFaptATIFpqWIqD7Tk"; // Access Tokenの値
-$access_token_secret = "xQ0QvOTLRmWnJmXWBd5ZBjHo4wGEl92b8VhCnbcp5I"; // Access Token Secretの値
+$screen_name = "--"; //botのid名
+$consumer_key = ""; // Consumer keyの値
+$consumer_secret = ""; // Consumer secretの値
+$access_token = ""; // Access Tokenの値
+$access_token_secret = ""; // Access Token Secretの値
 
 //高度な設定
 $replyLoopLimit = 3; //リプライのループを防ぐための設定です。大体3回くらいで会話が止まります（多分……）
